3aaa02dd5bd32b42c1657d0ec6d77085350439f5
zuowangda <wangda.zuo@gmail.com>
2013-07-18 21:57:33 -0400

    separated the trace_vy

--- advection.h
+++ advection.h
@@ -4,6 +4,8 @@ void semi_Lagrangian(PARA_DATA *para, REAL **var, int var_type,
                      REAL *d, REAL *d0);
 
 int trace_vx(PARA_DATA *para, REAL **var, int var_type, REAL *d, REAL *d0, int **BINDEX);
+int trace_vy(PARA_DATA *para, REAL **var, int var_type, REAL *d, REAL *d0, int **BINDEX);
+
 
 void XLOCATION(PARA_DATA *para, REAL **var,  REAL *flag, REAL *x, REAL u0, int i, int j, int k,  REAL *OL, int *OC, int *LOC , int *COOD);
 void YLOCATION(PARA_DATA *para, REAL **var,  REAL *flag, REAL *y, REAL v0, int i, int j, int k,  REAL *OL, int *OC, int *LOC , int *COOD);
