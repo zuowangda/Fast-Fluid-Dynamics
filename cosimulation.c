aa6ea1651bcbf684a77af2540924d912d2a67bcb
zuowangda <wangda.zuo@gmail.com>
2013-07-23 14:45:29 -0400

    Working but need to clean

--- cosimulation.c
+++ cosimulation.c
@@ -1,7 +1,116 @@
+//#include "stdafx.h"
+#include <windows.h>
+#include <stdio.h>
+
+
 #include "data_structure.h"
+#include "utility.h"
+#include "resource.h"
+
+
+typedef struct {
+  double number0;
+  double number1;
+  int command;
+} ReceivedDataFormat;
+
+typedef struct {
+  int feedback;
+} FFDFeedback;
+
+typedef struct {
+  double number0;
+  double number1;
+  int command;
+}FFDSend;
+
+typedef struct {
+  int feedback;
+}DymolaFeedback;
+
+//Global Variables:
+ReceivedDataFormat received;
+DymolaFeedback dymolaFeedback;
+
+//const char fFDRecvDataCh1[] = "FFDRecvDataCh1";         //Ch1: Dymola to FFD   Need define it in caption
+TCHAR dymolaRecvFeedbackCh1[] = TEXT("DymolaRecvFeedbackCh1");    
+//const char fFDRecvFeedbackCh2[] = "FFDRecvFeedbackCh2"; //Ch2: FFD to Dymola   Need define it in caption
+TCHAR dymolaRecvDataCh2[] = TEXT("DymolaRecvDataCh2");
+
+//******CHANNEL ONE DYMOLA TO FFD
+
+
+
+void sendFeedbackToDymolaCh1(FFDFeedback fFDFeedback){
+  HWND hSendWindow;
+  HWND hRecvWindow;
+  COPYDATASTRUCT sendFeedback;
+
+  hSendWindow = GetConsoleWindow ();        //get handel of two windows
+  if (hSendWindow == NULL) {
+    printf("%s\n", "self handel not found");  
+  }
+
+  hRecvWindow = FindWindow(NULL, dymolaRecvFeedbackCh1);
+  while(hRecvWindow == NULL){
+    Sleep(500);                                     //*******set waiting time
+    hRecvWindow = FindWindow(NULL, dymolaRecvFeedbackCh1);
+  }
+  sendFeedback.cbData = sizeof(fFDFeedback);            //set data to "package"
+  sendFeedback.lpData = &fFDFeedback;
+  SendMessage(hRecvWindow, WM_COPYDATA, (WPARAM)hSendWindow, (LPARAM)&sendFeedback); //send data
+}
 
 /******************************************************************************
-  Read data that is written by other program
+| Receive data from Windows message
+******************************************************************************/
+BOOL CALLBACK receive_data_dialog(HWND hwndDlg, UINT message, 
+                                  WPARAM wParam, LPARAM lParam)
+{
+  PCOPYDATASTRUCT pcds;
+
+  // Hide the dialog window
+  ShowWindow(hwndDlg, SW_HIDE);
+
+  switch (message)
+  {
+    case WM_CLOSE:
+      EndDialog(hwndDlg, 0);
+      break;
+    case WM_COPYDATA:
+      pcds = (PCOPYDATASTRUCT)lParam;
+      // If the size matches, copy the data
+      if (pcds->cbData == sizeof(received)){
+        memcpy_s(&received, sizeof(received), pcds->lpData, pcds->cbData);
+        EndDialog(hwndDlg, 0);
+      }
+  }
+
+  return FALSE;
+}
+
+/******************************************************************************
+| Receive data from the other program
+******************************************************************************/
+void receive_data(){
+  FFDFeedback ffd_feed_back;
+  char msg[500];
+
+  // Receive the data through the message
+  DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DATA), NULL, receive_data_dialog);
+  
+  sprintf(msg, "cosimulation.c: Received data: %d, %f, %f ", 
+          received.command, received.number0, received.number1);
+  ffd_log(msg, FFD_NORMAL);
+
+  // Send feedback for successfully receive
+  ffd_feed_back.feedback = 1;
+  sendFeedbackToDymolaCh1(ffd_feed_back);
+
+} // End of receive_data()
+
+/******************************************************************************
+  Read the data send from the other program
 ******************************************************************************/
 int read_cosimulation_data(PARA_DATA *para, REAL **var)
 {
@@ -11,20 +120,66 @@ int read_cosimulation_data(PARA_DATA *para, REAL **var)
   int IMAX = imax+2, IJMAX = (imax+2)*(jmax+2);
   REAL feak[1];
 
-  feak[0] = 10;
+  // Receive data from other program
+  receive_data();
 
-  /*--------------------------------------------------------------------------
-  | The following code is to be modified by the users
-  --------------------------------------------------------------------------*/
-  for(j=0; j<=jmax+1; j++)
-    for(k=0; k<=kmax+1; k++)
-      var[TEMPBC][IX(imax+1,j,k)]= feak[0];
-
-  /*-------------------------------------------------------------------------
-  | The above code is to be modified by the users
-  -------------------------------------------------------------------------*/
+  ffd_log("cosimulation.c: Sccessfully read data from the cosimulation program", FFD_NORMAL);
   return 0;
 }
+
+/******************************************************************************
+|
+******************************************************************************/
+BOOL CALLBACK RecvDymolaFeedbackDialogCh2(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+  PCOPYDATASTRUCT pcds; 
+  ShowWindow(hwndDlg, SW_HIDE);
+
+  switch (message){
+
+  case WM_CLOSE:
+      EndDialog(hwndDlg, 0);
+      break;
+
+    case WM_COPYDATA:
+      pcds = (PCOPYDATASTRUCT)lParam;
+      // If the size matches
+      if (pcds->cbData == sizeof(dymolaFeedback)){
+        memcpy_s(&dymolaFeedback, sizeof(dymolaFeedback), pcds->lpData, pcds->cbData);
+        EndDialog(hwndDlg, 0);
+      }
+  }
+  return FALSE;
+}
+
+int sendDataToDymolaCh2(FFDSend fFDSend){
+  HWND hSendWindow;
+  HWND hRecvWindow ;
+  COPYDATASTRUCT sendData;
+
+  hSendWindow = GetConsoleWindow ();        //get handel of two windows
+  if (hSendWindow == NULL) {
+    printf("%s\n", "self handel not found");  
+    system("pause"); 
+  }
+
+  hRecvWindow = FindWindow(NULL, dymolaRecvDataCh2);
+  while(hRecvWindow == NULL){
+    Sleep(500);                                     //*******set waiting time
+    hRecvWindow = FindWindow(NULL, dymolaRecvDataCh2);
+  }
+  sendData.cbData = sizeof(fFDSend);            //set data to "package"
+  sendData.lpData = &fFDSend;
+  SendMessage(hRecvWindow, WM_COPYDATA, (WPARAM)hSendWindow, (LPARAM)&sendData); //send data
+
+  //run recvfeedback close dialog
+  DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_FEEDBACK), NULL, RecvDymolaFeedbackDialogCh2);
+  if(dymolaFeedback.feedback==1){
+    return 1;
+  }else {
+    return 0;
+  }
+}
+
 
 /******************************************************************************
   Write data that will be read by other program
@@ -35,20 +190,22 @@ int write_cosimulation_data(PARA_DATA *para, REAL **var)
   int imax = para->geom->imax, jmax = para->geom->jmax;
   int kmax = para->geom->kmax;
   int IMAX = imax+2, IJMAX = (imax+2)*(jmax+2);
+  int check;
+  FFDSend fFDSend1;
 
-  REAL feak[1];
+  
+      //prepare data to send
+  fFDSend1.number0 = 888.666;
+  fFDSend1.number1 = 666.888;
+  fFDSend1.command = 123456;
 
-  /*--------------------------------------------------------------------------
-  | The following code is to be modified by the users
-  --------------------------------------------------------------------------*/
-  feak[0] = var[VX][IX(1,1,1)];
-
-  /*-------------------------------------------------------------------------
-  | The above code is to be modified by the users
-  -------------------------------------------------------------------------*/
+  check = sendDataToDymolaCh2(fFDSend1);
+  if (check==0){
+    ffd_log("No feedback from the other program", FFD_ERROR);
+  }
 
 
-  return 1;
+  return 0;
 }
 
 
