15de3eff49f4c09244e893a8e344cb54363c1372
zuowangda <wangda.zuo@gmail.com>
2013-08-06 01:59:59 -0400

    added comments

--- timing.c
+++ timing.c
@@ -1,14 +1,30 @@
-#include <stdio.h> 
-#include <time.h>
+///////////////////////////////////////////////////////////////////////////////
+///
+/// \file   time.c
+///
+/// \brief  Subroutines for timing
+///
+/// \author Mingang Jin, Qingyan Chen
+///         Purdue University
+///         Jin55@purdue.edu, YanChen@purdue.edu
+///         Wangda Zuo
+///         University of Miami
+///         W.Zuo@miami.edu
+///
+/// \date   8/3/2013
+///
+///////////////////////////////////////////////////////////////////////////////
 
-#include "data_structure.h"
 #include "timing.h"
 
-/******************************************************************************
-| Calculate the current time   
-******************************************************************************/
-void timing(PARA_DATA *para)
-{
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the simulation time and time ratio
+///
+///\param para Pointer to FFD parameters
+///
+///\return No return needed
+///////////////////////////////////////////////////////////////////////////////
+void timing(PARA_DATA *para) {
   double cputime;
 
   para->mytime->t += para->mytime->dt;
@@ -19,5 +35,4 @@ void timing(PARA_DATA *para)
 
   printf("Phyical time=%.1f s, CPU time=%.3f s, Time Ratio=%.4f\n", 
          para->mytime->t, cputime, para->mytime->t/cputime);
-
 } // End of timing( )
