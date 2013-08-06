66b5bae73c82f458732475d21fcc7622ff15762f
zuowangda <wangda.zuo@gmail.com>
2013-08-05 22:09:22 -0400

    added comments

--- geometry.c
+++ geometry.c
@@ -1,66 +1,130 @@
 ///////////////////////////////////////////////////////////////////////////////
-//
-// Filename: geometry.c
-//
-// Task: Functions for calculation related to geometry
-//
-// Modification history:
-// 7/10/2013 by Wangda Zuo: First implementation
-//
+///
+/// \file   diffusion.c
+///
+/// \brief  Calculate the geometry related information
+///
+/// \author Wangda Zuo
+///         University of Miami
+///         W.Zuo@miami.edu
+///         Mingang Jin, Qingyan Chen
+///         Purdue University
+///         Jin55@purdue.edu, YanChen@purdue.edu
+///
+/// \date   8/3/2013
+///
 ///////////////////////////////////////////////////////////////////////////////
-#include <math.h>
 
-#include "data_structure.h"
 #include "geometry.h"
 
-/******************************************************************************
-| Calcualte the XY area of cell (i,j,k)
-******************************************************************************/
-REAL area_xy(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX)
-{
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the XY area of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Area of XY surface
+///////////////////////////////////////////////////////////////////////////////
+REAL area_xy(PARA_DATA *para, REAL **var, int i, int j, int k, 
+             int IMAX, int IJMAX) {
   return length_x(para, var, i, j, k, IMAX, IJMAX) 
        * length_y(para, var, i, j, k, IMAX, IJMAX);
-}
+} // End of area_xy()
 
-/******************************************************************************
-| Calcualte the YZ area of cell (i,j,k)
-******************************************************************************/
-REAL area_yz(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX)
-{
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the YZ area of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Area of YZ surface
+///////////////////////////////////////////////////////////////////////////////
+REAL area_yz(PARA_DATA *para, REAL **var, int i, int j, int k, 
+             int IMAX, int IJMAX) {
   return length_y(para, var, i, j, k, IMAX, IJMAX) 
        * length_z(para, var, i, j, k, IMAX, IJMAX);
-}
+} // End of area_yz();
 
-/******************************************************************************
-| Calcualte the ZX area of cell (i,j,k)
-******************************************************************************/
-REAL area_zx(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX)
-{
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the ZX area of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Area of ZX surface
+///////////////////////////////////////////////////////////////////////////////
+REAL area_zx(PARA_DATA *para, REAL **var, int i, int j, int k, 
+             int IMAX, int IJMAX) {
   return length_z(para, var, i, j, k, IMAX, IJMAX) 
        * length_x(para, var, i, j, k, IMAX, IJMAX);
-}
+} // End of area_zx()
 
 
-/******************************************************************************
-| Calcualte the length in X of cell (i,j,k)
-******************************************************************************/
-REAL length_x(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX)
-{
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the X-length of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Length in X-direction
+///////////////////////////////////////////////////////////////////////////////
+REAL length_x(PARA_DATA *para, REAL **var, int i, int j, int k, 
+              int IMAX, int IJMAX) {
   return fabs(var[GX][IX(i,j,k)]-var[GX][IX(i-1,j,k)]); 
-}
+} // End of length_x()
 
-/******************************************************************************
-| Calcualte the length in Y of cell (i,j,k)
-******************************************************************************/
-REAL length_y(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX)
-{
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the Y-length of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Length in Y-direction
+///////////////////////////////////////////////////////////////////////////////
+REAL length_y(PARA_DATA *para, REAL **var, int i, int j, int k,
+              int IMAX, int IJMAX) {
   return fabs(var[GY][IX(i,j,k)]-var[GY][IX(i,j-1,k)]); 
-}
+} // End of length_y()
 
-/******************************************************************************
-| Calcualte the length in Z of cell (i,j,k)
-******************************************************************************/
-REAL length_z(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX)
-{
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the Z-length of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Length in Z-direction
+///////////////////////////////////////////////////////////////////////////////
+REAL length_z(PARA_DATA *para, REAL **var, int i, int j, int k,
+              int IMAX, int IJMAX) {
   return fabs(var[GZ][IX(i,j,k)]-var[GZ][IX(i,j,k-1)]); 
-}
+} // End of length_z()
--- geometry.h
+++ geometry.h
@@ -1,22 +1,117 @@
 ///////////////////////////////////////////////////////////////////////////////
-//
-// Filename: geometry.h
-//
-// Task: Head file of geometry.c
-//
-// Modification history:
-// 7/10/2013 by Wangda Zuo: First implementation
-//
+///
+/// \file   diffusion.h
+///
+/// \brief  Calculate the geometry related information
+///
+/// \author Wangda Zuo
+///         University of Miami
+///         W.Zuo@miami.edu
+///         Mingang Jin, Qingyan Chen
+///         Purdue University
+///         Jin55@purdue.edu, YanChen@purdue.edu
+///
+/// \date   8/3/2013
+///
 ///////////////////////////////////////////////////////////////////////////////
+#ifndef _GEOMETRY_H
+#define _GEOMETRY_H
+#endif
 
+#ifndef _DATA_STRUCTURE_H
+#define _DATA_STRUCTURE_H
+#include "data_structure.h"
+#endif
+
+#include <math.h>
+
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the XY area of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Area of XY surface
+///////////////////////////////////////////////////////////////////////////////
 REAL area_xy(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX);
 
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the YZ area of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Area of YZ surface
+///////////////////////////////////////////////////////////////////////////////
 REAL area_yz(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX);
 
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the ZX area of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Area of ZX surface
+///////////////////////////////////////////////////////////////////////////////
 REAL area_zx(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX);
 
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the X-length of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Length in X-direction
+///////////////////////////////////////////////////////////////////////////////
 REAL length_x(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX);
 
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the Y-length of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Length in Y-direction
+///////////////////////////////////////////////////////////////////////////////
 REAL length_y(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX);
 
-REAL length_z(PARA_DATA *para, REAL **var, int i, int j, int k, int IMAX, int IJMAX);
+///////////////////////////////////////////////////////////////////////////////
+/// Calculate the Z-length of control volume (i,j,k)
+///
+///\param para Pointer to FFD parameters
+///\param var Pointer to FFD simulation variables
+///\param i I-index of the control volume
+///\param j J-index of the control volume
+///\param K K-index of the control volume
+///\param IMAX Value of imax+2
+///\param IMAX Value of (imax+2)*(jmax+2)
+///
+///\return Length in Z-direction
+///////////////////////////////////////////////////////////////////////////////
+REAL length_z(PARA_DATA *para, REAL **var, int i, int j, int k,
+              int IMAX, int IJMAX);
