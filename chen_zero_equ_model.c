1058c5a8d9e6805970ffbd0f49921fcbd956fd51
zuowangda <wangda.zuo@gmail.com>
2013-08-16 21:54:40 -0400

    Corrected the function for z direction

--- chen_zero_equ_model.c
+++ chen_zero_equ_model.c
@@ -34,25 +34,24 @@ REAL nu_t_chen_zero_equ(PARA_DATA *para, REAL **var, int i, int j, int k) {
   REAL nu_t, l, lx, lx1, lx2, ly, ly1, ly2, lz, lz1, lz2;
   REAL *x = var[X], *y = var[Y], *z = var[Z];
   REAL *u = var[VX], *v = var[VY], *w = var[Z];
-  int imax = para->geom->imax, jmax = para->geom->jmax;
-  int kmax = para->geom->kmax;
+  int imax = para->geom->imax, jmax = para->geom->jmax,
+      kmax = para->geom->kmax;
   int IMAX = imax+2, IJMAX = (imax+2)*(jmax+2);
   
   lx1 = x[IX(i,j,k)] - x[IX(0,j,k)];
   lx2 = x[IX(imax+1,j,k)] - x[IX(i,j,k)];
-
-  lz = 0;
-  lz1 = 0;
-  lz2 = 0;
-
   lx = lx1 < lx2 ? lx1 : lx2;
 
   ly1 = y[IX(i,j,k)] - y[IX(i,0,k)];
   ly2 = y[IX(i,jmax,k)] - y[IX(i,j,k)];
   ly = ly1 < ly2 ? ly1 : ly2;
 
-  l = lx < ly ? lx : ly;
+  lz1 = z[IX(i,j,k)] - z[IX(i,j,0)];
+  lz2 = z[IX(i,j,kmax+1)] - z[IX(i,j,k)];
+  lz = lz1 < lz2 ? lz1 : lz2;
 
+  l = lx < ly ? lx : ly;
+  l = lz < l ? lz : l;
   nu_t = para->prob->chen_a * l
        * (REAL)sqrt( u[IX(i,j,k)]*u[IX(i,j,k)]
                     +v[IX(i,j,k)]*v[IX(i,j,k)]
