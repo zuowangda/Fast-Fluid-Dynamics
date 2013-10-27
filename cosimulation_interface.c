e3460418e89caa03b5af8d95ba25d563c6372dda
zuowangda <wangda.zuo@gmail.com>
2013-10-27 16:58:39 -0400

    Correct grammar error

--- cosimulation_interface.c
+++ cosimulation_interface.c
@@ -309,7 +309,7 @@ int write_cosim_data(PARA_DATA *para, REAL **var) {
   if(para->cosim->para->sha==1) {
     ffd_log("\tTemperature of the shade:", FFD_NORMAL);
     for(i=0; i<para->cosim->para->nConExtWin; i++) {
-      //Waring: The shade feature is to be implemented
+      //Fixme: The shade feature is to be implemented
       para->cosim->ffd->TSha[i] = 20 + 273.15; 
       sprintf(msg, "\t\tSurface %d: %f[K]\n",
               i, para->cosim->ffd->TSha[i]);
